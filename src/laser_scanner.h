6f08ac03a0437fbac9541860ea1cc0da9525d5d2
AndreaCarraro26 <andrea.carraro26@gmail.com>
2016-05-02 11:01:41 +0200

    Update laser_scanner.h
    Aggiunto reproject()

--- src/laser_scanner.h
+++ src/laser_scanner.h
@@ -1,15 +1,16 @@
-#ifndef laser_scanner_H
-#define laser_scanner_H
- 
 #include <osg/Group>
 #include <pcl/point_types.h>
-#include <pcl/common/projection_matrix.h>		//per PointCloud
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-cv::Mat get_pic(osg::ref_ptr<osg::Geode>, osg::Matrixd &);
-osg::ref_ptr<osg::Geode> scan_scene(osg::ref_ptr<osg::Node>, float, osg::Vec4d&, osg::Vec4d&);
+osg::ref_ptr<osg::Geode> scan_scene(osg::ref_ptr<osg::Node>, float, osg::Vec4d*, osg::Vec4d*);
 void convert_to_3d (cv::Mat , std::vector<double> , std::vector<double> , pcl::PointCloud<pcl::PointXYZ>::Ptr );
-
-#endif
+cv::Mat get_pic(osg::ref_ptr<osg::Geode>, osg::Matrix &);
+cv::Mat reproject(osg::ref_ptr<osg::Node>, float, osg::Vec4d&, osg::Vec4d&);
+	
