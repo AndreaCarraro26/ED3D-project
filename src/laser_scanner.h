08f40fda3293d62f454e8c6020ed4d9e07274535
AndreaCarraro26 <andrea.carraro26@gmail.com>
2016-05-15 12:04:32 +0200

    Update laser_scanner.h
    Cambiato la chiamata del metodo reproject (eliminati i riferimenti ai piani laser)

--- src/laser_scanner.h
+++ src/laser_scanner.h
@@ -1,17 +1,18 @@
+#include "stdafx.h"
+
 #include <osg/Group>
 #include <pcl/point_types.h>
-#include <pcl/common/projection_matrix.h>	/*
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/voxel_grid.h>
-#include <pcl/io/pcd_io.h>*/
+#include <pcl/io/pcd_io.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 osg::ref_ptr<osg::Geode> scan_scene(osg::ref_ptr<osg::Node>, float, osg::Vec4d*, osg::Vec4d*);
-void convert_to_3d (cv::Mat , std::vector<double> , std::vector<double> , pcl::PointCloud<pcl::PointXYZ>::Ptr );
+void convert_to_3d (cv::Mat, double, std::vector<double> , std::vector<double> , pcl::PointCloud<pcl::PointXYZ>::Ptr );
 cv::Mat get_pic(osg::ref_ptr<osg::Geode>, osg::Matrix &);
-cv::Mat reproject(osg::ref_ptr<osg::Node>, float, osg::Vec4d&, osg::Vec4d&);
+cv::Mat reproject(osg::ref_ptr<osg::Node>, float);
 	
